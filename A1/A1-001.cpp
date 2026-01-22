8ede7f254f8f77ab640e320722c9665739951577
exoworldgd <exoworldgd@gmail.com>
2026-01-22 22:43:53 +0700

    Add files via upload

---
+++ A1/A1-001.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#define exoworldgd cin.tie(0)->sync_with_stdio(0),cout.tie(0)
+#define int long long
+using namespace std;
+string a,b;
+signed main(void){
+    exoworldgd;
+    cin>>a>>b,cout<<"Hello "<<a<<' '<<b<<'\n'<<a[0]<<a[1]<<b[0]<<b[1];
+}
